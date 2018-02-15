c357dd24304ae1bcbd2515f1b5b33fad7ee56d53
Allan Peng <potette007@gmail.com>
2018-02-15 18:31:47 -0500

    add tables

--- cuckoo.c
+++ cuckoo.c
@@ -11,7 +11,8 @@ typedef struct {
     int size;
     float load_factor;
     int num_buckets;
-    entry** entries;
+    // lmao
+    entry*** entries;
 } hashtable;
 
 hashtable *
@@ -20,10 +21,14 @@ init_hashtable(int size) {
     init->size = size;
     init->num_buckets = 0;
     init->load_factor = 0.5;
-    init->entries = malloc(size * sizeof(entry*));
+    init->entries = malloc( 2 * sizeof(entry**));
     int i;
-    for(i = 0; i < size; i++) {
-        init->entries[i] = NULL;
+    int j;
+    for (i = 0; i < 2; i++) {
+        init->entries[i] = malloc(size * sizeof(entry*));
+        for(j = 0; j < size; j++) {
+            init->entries[i][j] = NULL;
+        }
     }
     return init;
 }
@@ -33,8 +38,8 @@ get(hashtable *table, char *key) {
     int hasher[] = {0,0};
     hash(key, hasher);
     for (int i = 0; i < 2; i++) {
-        if (table->entries[hasher[i]] != NULL) {
-            entry *buck = table->entries[hasher[i]];
+        if (table->entries[i][hasher[i]] != NULL) {
+            entry *buck = table->entries[i][hasher[i]];
             if (!strcmp(buck->key, key)) {
                 return buck->value;
             }
@@ -48,10 +53,10 @@ delete(hashtable *table, char *key) {
     int hasher[] = {0,0};
     hash(key, hasher);
     for (int i = 0; i < 2; i++) {
-        if (table->entries[hasher[i]] != NULL) {
-            entry *buck = table->entries[hasher[i]];
+        if (table->entries[i][hasher[i]] != NULL) {
+            entry *buck = table->entries[i][hasher[i]];
             if (!strcmp(buck->key, key)) {
-                buck = NULL;
+                *(&buck) = NULL;
                 table->num_buckets --;
                 return 1;
             }
@@ -66,10 +71,35 @@ insert(hashtable *table, char *key, char *value) {
     int hasher[] = {0,0};
     hash(key, hasher);
     for (int i = 0; i < 2; i++) {
-        if (table->entries[hasher[i]] == NULL) {
-            *table->entries[hasher[i]] = entry{key, value};
+        if (table->entries[i][hasher[i]] == NULL) {
+            *table->entries[i][hasher[i]] = entry{key, value};
+            return 1;
+        } else {
+            if (!strcmp(table->entries[i][hasher[i]]->key, key)) {
+                table->entries[i][hasher[i]]->value = value;
+                return 1;
+            }
+        }
+    }
+    char *temp_key;
+    char *temp_value;
+    entry *curr_entry;
+    int i = 0;
+    while (1) {
+        curr_entry = table->entries[i % 2][hasher[i % 2]]
+        if (curr_entry != NULL) {
+            temp_key = curr_entry->key;
+            temp_value = curr_entry->value;
+        }
+        *table->entries[hasher[i %2]] = key;
+        *table->entries[hasher[i %2]] = value;
+        if (curr_entry == NULL) {
             return 1;
         }
+        key = temp_key;
+        value = temp_value;
+        hash(key, hasher);
+        i++;
     }
 }
 
