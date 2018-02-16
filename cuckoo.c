06ffeb5a470c03cda56f2b52e7ac411685cf0ec0
Allan Peng <potette007@gmail.com>
2018-02-16 13:49:54 -0500

    fixes

--- cuckoo.c
+++ cuckoo.c
@@ -38,23 +38,73 @@ init_hashtable(int size) {
     return init;
 }
 
+int
+_put(entry ***entries, char *key, char *value) {
+    int SIZE = sizeof(*entries) / sizeof(**entries);
+    int hasher[] = {0,0};
+    hash(key, hasher, SIZE);
+    for (int i = 0; i < 2; i++) {
+        if (entries[i][hasher[i]] == NULL) {
+            entry *new_struct = malloc(sizeof(entry));
+            new_struct->key = key;
+            new_struct->value = value;
+            entries[i][hasher[i]] = new_struct;
+            return 1;
+        } else {
+            if (!strcmp(entries[i][hasher[i]]->key, key)) {
+                entries[i][hasher[i]]->value = value;
+                return 0;
+            }
+        }
+    }
+    char *temp_key;
+    char *temp_value;
+    entry *curr_entry;
+    int i = 0;
+    while (1) {
+        curr_entry = entries[i % 2][hasher[i % 2]];
+        if (curr_entry == NULL) {
+            entry *new_struct = malloc(sizeof(entry));
+            new_struct->key = key;
+            new_struct->value = value;
+            entries[i % 2 ][hasher[i % 2]] = new_struct;
+            return 1;
+        }
+        temp_key = curr_entry->key;
+        temp_value = curr_entry->value;
+        entries[i % 2][hasher[i %2]]->key = key;
+        entries[i % 2][hasher[i %2]]->value = value;
+
+        key = temp_key;
+        value = temp_value;
+        hash(key, hasher, SIZE);
+        i++;
+    }
+}
+
 void
 resize(hashtable *table) {
-    hashtable *new_table = init_hashtable(table->size * 2);
+    entry ***new_entry = malloc(2 * sizeof(entry**));
     int i;
     int j;
     for (i = 0; i < 2; i++) {
+        new_entry[i] = malloc(2 * table->size * sizeof(entry*));
+        for(j = 0; j < 2 * table->size; j++) {
+            new_entry[i][j] = NULL;
+        }
+    }
+
+    for (i = 0; i < 2; i++) {
         for (j = 0; j < table->size; j++) {
             if (table->entries[i][j] != NULL) {
-                put(new_table, table->entries[i][j]->key, table->entries[i][j]->value);
+                _put(new_entry, table->entries[i][j]->key, table->entries[i][j]->value);
                 free(table->entries[i][j]);
             }
         }
         free(table->entries[i]);
     }
     free(table->entries);
-    free(table);
-    *(&table) = new_table;
+    table->entries = new_entry;
 }
 
 
@@ -98,47 +148,8 @@ put(hashtable *table, char *key, char *value) {
     if (table->num_buckets > table->load_factor * table->size * 2) {
         resize(table);
     }
-    int hasher[] = {0,0};
-    hash(key, hasher, table->size);
-    for (int i = 0; i < 2; i++) {
-        if (table->entries[i][hasher[i]] == NULL) {
-            entry *new_struct = malloc(sizeof(entry));
-            new_struct->key = key;
-            new_struct->value = value;
-            table->entries[i][hasher[i]] = new_struct;
-            table->num_buckets++;
-            return 1;
-        } else {
-            if (!strcmp(table->entries[i][hasher[i]]->key, key)) {
-                table->entries[i][hasher[i]]->value = value;
-                return 1;
-            }
-        }
-    }
-    char *temp_key;
-    char *temp_value;
-    entry *curr_entry;
-    int i = 0;
-    while (1) {
-        curr_entry = table->entries[i % 2][hasher[i % 2]];
-        if (curr_entry == NULL) {
-            entry *new_struct = malloc(sizeof(entry));
-            new_struct->key = key;
-            new_struct->value = value;
-            table->entries[i % 2 ][hasher[i % 2]] = new_struct;
-            table->num_buckets++;
-            return 1;
-        }
-        temp_key = curr_entry->key;
-        temp_value = curr_entry->value;
-        table->entries[i % 2][hasher[i %2]]->key = key;
-        table->entries[i % 2][hasher[i %2]]->value = value;
-
-        key = temp_key;
-        value = temp_value;
-        hash(key, hasher, table->size);
-        i++;
-    }
+    table->size += _put(table->entries, key, value);
+    return 1;
 }
 
 int
